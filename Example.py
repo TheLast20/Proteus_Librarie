2f4651effafc00242a6b3e85674e574ed1614c3f
TheLast20 <67993682+TheLast20@users.noreply.github.com>
2020-10-04 23:11:03 -0500

    Update Example.py

--- Example.py
+++ Example.py
@@ -1,12 +1,19 @@
 import Proteus as Pt
 
+#Create de system
 system = Pt.Signal(10,0.0001)
 
+#Add de signal PWM
 system.Add_PWM(low_voltage=0,high_voltage=5,pulse_width=75,period=2)
+#Generate de Graph
 system.Generate_Graph()
+#Generate File
 system.Generate_File("signal PWM")
 
+#Use de atribute Clear_Signal 
 system.Clear_Signal()
+
+#Repit Process
 system.Add_Sinusoidal(Amplitud=5,Frecuency=60)
 system.Add_Sinusoidal(Amplitud=2,Frecuency=25)
 system.Add_Sinusoidal(Amplitud=3,Frecuency=100,start_time=1/60)
