d652b875c3137840314d739749477ea61ca60c20
TheLast20 <67993682+TheLast20@users.noreply.github.com>
2020-10-04 23:01:10 -0500

    Delete Example_FM.py

--- Example_FM.py
+++
@@ -1,8 +0,0 @@
-import Proteus as Pt
-
-system = Pt.Signal(10,0.0001)
-system.Generate_FM_Signal(amplitud=5,frecuency1=60,frecuency2=130,period=0.05)
-system.Generate_Graph(min_x=0,max_x=0.2)
-system.Generate_File("signal1")
-
-
